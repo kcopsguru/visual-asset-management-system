4dc2f74af99d8a78f63d7d5185a46ce39669f19e
Kenny Cheang <kenny.cheang@opsguru.io>
2023-08-10 09:59:13 -0700

    updated deployment after merge

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 export AWS_REGION=us-east-1
-export STACK_NAME=vams-playground
+export STACK_NAME=tst1
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 npm run deploy.dev adminEmailAddress=kenny.cheang@opsguru.io
